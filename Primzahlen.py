075027ff297aca28eb02170707939c49afe9e2ea
Emma Stellwag <emmastellwag@Emmas-MacBook-Pro.local>
2018-02-04 15:27:28 +0100

    fertig

--- Primzahlen.py
+++ Primzahlen.py
@@ -1,1 +1,27 @@
-n = input("Bis wohin sollen Primzahlen gesucht werden?")
+n = (input("Bis wohin sollen Primzahlen gesucht werden?")+1)
+numbers=[[],[]]
+prims = []
+for i in range(n):
+    numbers[0].append(i)
+    numbers[1].append(True)
+
+numbers[1][1] = False
+numbers[1][0] = False
+
+z = 2
+for z in range((n/2)):
+    if numbers[1][z] == True:
+        x = z + 1
+        while x<n:
+            if x % z == 0:
+                numbers[1][x] = False
+                x = x + 1
+            else:
+                x = x +1
+        z = z+1
+
+for t in range((n-1)):
+    if numbers[1][t] == True:
+        prims.append(numbers[0][t])
+print("Primzahlen:")
+print(prims)        
